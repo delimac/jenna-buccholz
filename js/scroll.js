2515b2543887ba98cf645d17859fec1035f601fa
Chris De Lima <delimac@hey.com>
2021-08-11 23:34:34 +0800

    added scroll js file

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+const pixelsTag = document.querySelector("div.pixels")
+
+// when user scrolls the page, update pixelsTag to how far the user scrolled
+pixelsTag.addEventListener("scroll", function () {
+  
+})
