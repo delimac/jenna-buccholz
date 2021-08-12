56408b39e693952212c0ea09c9569e8d35e3ad4a
Chris De Lima <delimac@hey.com>
2021-08-12 22:52:37 +0800

    added way to track pixel position when scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 const pixelsTag = document.querySelector("div.pixels")
 
 // when user scrolls the page, update pixelsTag to how far the user scrolled
-pixelsTag.addEventListener("scroll", function () {
-  
+document.addEventListener("scroll", function () {
+  const pixels = window.pageYOffset
+  pixelsTag.innerHTML = Math.floor(pixels)
 })
