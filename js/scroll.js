505af792d172e52b8ae47dca69ca8ab0c6db2052
Chris De Lima <delimac@hey.com>
2021-08-13 23:25:58 +0800

    added a progress bar, pt. 1

--- js/scroll.js
+++ js/scroll.js
@@ -5,3 +5,9 @@ document.addEventListener("scroll", function () {
   const pixels = window.pageYOffset
   pixelsTag.innerHTML = Math.floor(pixels)
 })
+
+// when user scrolls, make a progress bar that keeps track of the distanced
+// scrolled vertically in the page
+document.addEventListener("scroll", function () {
+  const pixels = window.pageYOffset
+})
